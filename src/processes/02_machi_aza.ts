dbeacf5fb599ecaa4f09f79dffdee7e23d68a2d5
Ko Nagase <geosanak@gmail.com>
2025-06-26 09:15:28 +0900

    Replace MachiAza chome zenkaku number to kanji number

--- src/processes/02_machi_aza.ts
+++ src/processes/02_machi_aza.ts
@@ -1,8 +1,20 @@
+import { kanji2number, number2kanji } from '@geolonia/japanese-numeral'
 import { SingleMachiAza } from "../data.js";
 import { MachiAzaData, MachiAzaPosData } from "../lib/ckan_data/machi_aza.js";
 import { projectABRData } from "../lib/proj.js";
 
 export function rawToMachiAza(raw: MachiAzaData | (MachiAzaData & MachiAzaPosData)): SingleMachiAza {
+  let kanjiChome = undefined;
+  if (raw.chome !== '') {
+    const numberChomePattern = /([０-９]+)(丁目?)/;
+    const matches = raw.chome.match(numberChomePattern);
+    if (!matches) {
+      kanjiChome = raw.chome;
+    } else if (matches.length === 3) {
+      const number = kanji2number(matches[1]);
+      kanjiChome = number2kanji(number) + matches[2];
+    }
+  }
   return {
     machiaza_id: raw.machiaza_id,
 
@@ -10,7 +22,7 @@ export function rawToMachiAza(raw: MachiAzaData | (MachiAzaData & MachiAzaPosDat
     oaza_cho_k: raw.oaza_cho_kana === '' ? undefined : raw.oaza_cho_kana,
     oaza_cho_r: raw.oaza_cho_roma === '' ? undefined : raw.oaza_cho_roma,
 
-    chome: raw.chome === '' ? undefined : raw.chome,
+    chome: kanjiChome,
     chome_n: raw.chome_number === '' ? undefined : parseInt(raw.chome_number, 10),
 
     koaza: raw.koaza === '' ? undefined : raw.koaza,
